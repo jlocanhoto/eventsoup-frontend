3945890f07b0d51d5db834d2e15ac79f29517ff9
João Lucas <jloc@cin.ufpe.br>
2017-06-18 20:53:56 -0300

    Began to update SelectPackageCompnent to materialize

---
+++ src/app/organizer/select-package/select-package.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-package',
+  templateUrl: './select-package.component.html',
+  styleUrls: ['./select-package.component.css']
+})
+export class SelectPackageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
