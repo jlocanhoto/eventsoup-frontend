91d5fdf1b365ae907e20e4bb2dedac0b7030d7b0
João Lucas <jloc@cin.ufpe.br>
2017-06-26 05:36:35 -0300

    Fixed bug with days letters of the calendar

--- src/app/organizer/create-event/create-event.component.ts
+++ src/app/organizer/create-event/create-event.component.ts
@@ -66,13 +66,24 @@ export class CreateEventComponent {
 		});
 
 		var that = this;
+		var weekdays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 
 		$('.datepicker').pickadate({
 			selectMonths: true, // Creates a dropdown to control month
 			selectYears: 10, // Creates a dropdown of 15 years to control year
+			onStart: function() {
+				$('.picker__weekday').each(function(index) {
+					$(this).text(weekdays[index]);
+				});
+			},
 			onClose: function() {
 				that.date = this.get('select');
 				console.log(that.date)
+			},
+			onSet: function() {
+				$('.picker__weekday').each(function(index) {
+					$(this).text(weekdays[index]);
+				});
 			}
 		});
 
