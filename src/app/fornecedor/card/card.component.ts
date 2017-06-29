245c921572832b2ff50b2c47e7e26fc9fbb464a1
Leonardo Alves <las3@cin.ufpe.br>
2017-06-29 18:13:40 -0300

    New Card layout

--- src/app/fornecedor/card/card.component.ts
+++ src/app/fornecedor/card/card.component.ts
@@ -9,33 +9,87 @@ declare let $:any
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  
+
   @Input() url = "";
   @Input() codigo = "";
   @Output() pronto = new EventEmitter();
   itens = []
-  tempo: Date = new Date(1,0,0,0,0,10);
+  tempo: Date = new Date('2017-02-29T06:05:01');
   maisItens: boolean = false
   botao: boolean = false
+  dia: number = this.tempo.getDate();
+  mes:string;
+  horario:string;
+  statusEntrega:string;
 
-  constructor(private fornecedor: FornecedorService) { 
+  constructor(private fornecedor: FornecedorService) {
     this.itens = fornecedor.getItens();
+    this.statusEntrega = 'Confirmado';
     this.timer()
     $(document).ready(function(){
       // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
       $('.modal').modal();
     });
+
+    this.getMes();
+    this.getHorario();
+
   }
 
   getItens() {
-    if (this.itens.length > 2) {
-      this.maisItens = true
-      return this.itens.slice(0,2);
-    }
     return this.itens
   }
 
-  
+  getHorario() {
+
+    this.horario = this.tempo.getHours() + ':' + this.tempo.getMinutes();
+    return  this.horario;
+  }
+
+
+  getMes() {
+    switch (this.tempo.getMonth()) {
+      case 1 :
+        this.mes = 'JAN';
+        break;
+      case 2 :
+        this.mes = 'FEV';
+        break;
+      case 3 :
+        this.mes = 'MAR';
+        break;
+      case 4 :
+        this.mes = 'ABR';
+        break;
+      case 5 :
+        this.mes = 'MAI';
+        break;
+      case 6 :
+        this.mes = 'JUN';
+        break;
+      case 7 :
+        this.mes = 'JUL';
+        break;
+      case 8 :
+        this.mes = 'AGO';
+        break;
+      case 9 :
+        this.mes = 'SPT';
+        break;
+      case 10 :
+        this.mes = 'OCT';
+        break;
+      case 11 :
+        this.mes = 'NOV';
+        break;
+      case 12 :
+        this.mes = 'DEZ';
+        break;
+      default :  this.mes = 'JAN';
+    }
+    return this.mes;
+  }
+
   timer() {
     setTimeout(() => {
       this.tempo.setSeconds(this.tempo.getSeconds() - 1)
